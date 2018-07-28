40ab2d65e76f4d48c9f1305a342d0e6681363255
bbong <star1231076@gmail.com>
2018-07-28 14:32:09 +0900

    안드로이드 문자열처리 ㅇㅇ

--- server/routes/apps.js
+++ server/routes/apps.js
@@ -32,7 +32,7 @@ router.get('/post', function(req, res, next) {
 
     appsDB.getPost((result) => {
         for (var i = 0; i < result.length; i++) 
-            result[i].text = result[i].text.replace(/(<([^>]+)>)/gi, '').replace('&nbsp;', '\n');
+            result[i].text = result[i].text.replace(/(<([^>]+)>)/gi, '').replace(/&nbsp;/gi, '\n');
         res.json(result)
     }, (error) => {
         res.status(200)
